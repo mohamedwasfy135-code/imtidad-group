f4c1f2d38aa7c4c46209d615ad99b93304f7061a
mohamedwasfy <mohamedfaiek07@gmail.com>
2026-09-04 21:14:07 +0300

    fix: expose supabase env vars in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,https://bzbrqhdvbpodsexopyqg.supabase.co
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6YnJxaGR2YnBvZHNleG9weXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1Mzg0MDEsImV4cCI6MjEwNDExNDQwMX0.mnp5wV6YNXA_4H_3_HQR6fUcVKtKqSDhzlNGprfFUV4,
+  },
 };
 
 export default nextConfig;
